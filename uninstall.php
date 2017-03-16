e947496d1190de4dbf09c5b4706b64544446b899
Sjur Sutterud Sagen <suttiz@gmail.com>
2017-03-16 14:22:16 +0100

    uninstall file added
    drops the custom db tables when plugin is uninstalled

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sjur Sutterud Sagen
+ * Date: 16.03.2017
+ * Time: 14.17
+ */
+
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+//drop custom database tables
+global $wpdb;
+
+$table_name_main = $wpdb->prefix . "prisliste_produkter";
+$table_name_product_category = $wpdb->prefix . "prisliste_kategorier";
+$table_name_product_ingredients = $wpdb->prefix . "prisliste_produkt_ingredienser";
+$table_name_product_allergens = $wpdb->prefix . "prisliste_produkt_allergener";
+
+$wpdb->query("DROP TABLE IF EXISTS $table_name_main");
+$wpdb->query("DROP TABLE IF EXISTS $table_name_product_category");
+$wpdb->query("DROP TABLE IF EXISTS $table_name_product_ingredients");
+$wpdb->query("DROP TABLE IF EXISTS $table_name_product_allergens");
