70d47d29d415c677620f116f28fa55284afaf840
Sjur Sutterud Sagen <suttiz@gmail.com>
2017-03-16 10:20:40 +0100

    Working on SQL
    2 of 4 create table sql done

--- prisliste.php
+++ prisliste.php
@@ -6,6 +6,62 @@ Author: Sjur Sutterud Sagen
 Version: 0.1
 */
 
+//check for security
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+//DB versioning
+global $prisliste_db_version;
+$prisliste_db_version = "1.0";
+
+//Function for creating the DB tables on plugin activation
+function prisliste_install() {
+
+    global $wpdb;//grabbing the wp database prefix in this install
+    global $prisliste_db_version;
+
+    //loading library for dbDelta function
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    $table_name_main = $wpdb->prefix . "prisliste";
+    $table_name_product_category = $wpdb->prefix . "prisliste_kategorier";
+    $table_name_product_ingredients = $wpdb->prefix . "prisliste_produkt_ingredienser";
+    $table_name_product_allergens = $wpdb->prefix . "prisliste_produkt_allergener";
+    $charset_collate = $wpdb->get_charset_collate();
+
+    //SQL query and table creating
+    $sql = "CREATE TABLE $table_name_product_category (
+      category_id mediumint(9) NOT NULL AUTO_INCREMENT,
+      category_name tinytext NOT NULL UNIQUE,
+      PRIMARY KEY  (category_id)
+    ) $charset_collate;
+
+    CREATE TABLE $table_name_main (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      category mediumint(9) NOT NULL,
+      name tinytext NOT NULL,
+      pris INT NOT NULL,
+      picture_url varchar(255) DEFAULT '' NOT NULL,
+      PRIMARY KEY  (id)
+    ) $charset_collate;
+
+    CREATE TABLE $table_name_product_allergens (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      
+    ) $charset_collate;
+    
+    CREATE TABLE $table_name_product_ingredients (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      product_id mediumint(9) NOT NULL
+    ) $charset_collate;
+    ";
+    dbDelta($sql);
+
+    add_option( 'prisliste_db_version', $prisliste_db_version );
+
+}
+
+
+
 add_action('admin_menu', 'prisliste_setup_menu');
 
 function prisliste_setup_menu() {
